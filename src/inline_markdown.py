e92471b967dc9f7ec5a8d434c002458cc2c21ca1
Tobi Ace <omoniyiadejengbe@gmail.com>
2024-12-08 15:26:12 +0100

    inline markdown text to textnodes added along with tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -99,3 +99,17 @@ def extract_markdown_links(text):
     link_regex = r"\[(.*?)\]\((https?://[^\s]+)\)"
     
     return re.findall(link_regex, text)
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    
+    nodes = split_nodes_image(nodes)
+    
+    nodes = split_nodes_link(nodes)
+    
+    return nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 
 class TestSplitNodesDelimiter(unittest.TestCase):
 
@@ -168,6 +168,24 @@ class TestSplitNodesDelimiter(unittest.TestCase):
                 TextNode(" with text that follows", TextType.TEXT),
             ],
             new_nodes,
+        )
+        
+    def test_text2nodes(self):
+        node = text_to_textnodes("This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ], node
         )
 
 
