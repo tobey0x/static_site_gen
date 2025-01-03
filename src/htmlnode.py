098e29b7b76bec11fd8371198fd461c9b743b91c
Tobi Ace <omoniyiadejengbe@gmail.com>
2025-01-03 18:36:09 +0100

    fix: ensure LeafNode renders correctly when tag is None

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,6 +26,7 @@ class LeafNode(HTMLNode):
             raise ValueError("All leaf nodes must have a value")
         if self.tag is None:
             return f'{self.value}'
+        
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
 
 
