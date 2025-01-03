1a71698bedc6c89d74fbe128f118640ab655d554
Tobi Ace <omoniyiadejengbe@gmail.com>
2025-01-03 18:34:18 +0100

    refactor text_node_to_html_node to use keyword arguments for LeafNode

--- src/textnode.py
+++ src/textnode.py
@@ -27,17 +27,18 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
-def text_node_to_html_node(text_node):
+def text_node_to_html_node(text_node: TextNode):
     if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, text_node.text)
+        return LeafNode(tag=None, value=text_node.text)
     if text_node.text_type == TextType.BOLD:
-        return LeafNode("b", text_node.text)
+        return LeafNode(tag="b", value=text_node.text)
     if text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", text_node.text)
+        return LeafNode(tag="i", value=text_node.text)
     if text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text)
+        return LeafNode(tag="code", value=text_node.text)
     if text_node.text_type == TextType.LINK:
-        return LeafNode("a", text_node.text, {"href": text_node.url})
+        return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+    
     raise ValueError(f"Invalid text type: {text_node.text_type}")
