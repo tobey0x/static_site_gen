340644bbca385e70c29c5fbd25b1200562e9f287
Tobi Ace <omoniyiadejengbe@gmail.com>
2025-01-09 01:49:54 +0100

    feat: add recursive directory copy function and initial CSS styles

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
 from textnode import TextNode
+from pathlib import Path
+import os
+import shutil
+
+def recursive_copy_directory(src, dest):
+    if not dest.exists():
+        dest.mkdir()
+    
+    for file in src.iterdir():
+        if file.is_file():
+            shutil.copy(file, dest)
+        else:
+            recursive_copy_directory(file, dest / file.name)
+        
+    
 
 
 def main():
-    text_obj = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(text_obj)
+    project_root = Path.cwd()
+    public_dir_in_dest = project_root / "public"
+    static_dir_in_src = project_root / "static"
+    
+    
+    if public_dir_in_dest.exists():
+        shutil.rmtree(public_dir_in_dest)
+        
+    recursive_copy_directory(static_dir_in_src, public_dir_in_dest)
 
 main()
